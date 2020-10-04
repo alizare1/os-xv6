7aa43205fe9094554a0994a77e309af207bb20c5
alizare1 <ali.zare2000123@gmail.com>
2020-10-04 17:06:26 +0330

    use write instead of printf

--- lcm.c
+++ lcm.c
@@ -20,6 +20,8 @@ int
 main(int argc, char *argv[])
 {
   int i, lcm, fd;
+  char w_buff[16];
+  static char digits[] = "0123456789";
   
   if (argc < 2) {
       printf(1, "lcm: No numbers were provided!\n");
@@ -31,7 +33,17 @@ main(int argc, char *argv[])
     lcm = (atoi(argv[i]) * lcm) / gcd(atoi(argv[i]), lcm);
 
   fd = open("lcm_result.txt", O_CREATE | O_WRONLY);
-  printf(fd, "%d\n", lcm);
+  
+  i = 0;
+  do {
+    w_buff[i++] = digits[lcm % 10];
+    lcm /= 10;
+  } while (lcm > 0);
+
+  while(--i >= 0)
+    write(fd, &w_buff[i], 1);
+  write(fd, "\n", 1);
+
   close(fd);
 
   exit();
